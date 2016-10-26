f4dfcc91e0d1e12ef1f9aa8ce8f0fd0343ca5f2e
fvf1361 <fvf_1361@yahoo.com>
2016-10-25 19:13:43 -0600

    Add files via upload

---
+++ server.R
@@ -0,0 +1,71 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define server logic required to draw a plot
+shinyServer(function(input, output) {
+    
+    library(UsingR)
+    data("father.son")
+    fs <- as.data.frame(father.son)
+    
+    
+  output$distPlot <- renderPlot({
+    
+    xmin <- min(input$inmodel)
+    xmax <- max(input$inmodel)
+    fs <- fs[(father.son$fheight>=xmin)&(father.son$fheight<=xmax),]
+    x <- fs$fheight
+    y <- fs$sheight
+    plot(x,y, xlab = "father's height (inches)", ylab = "son's height (inches)", xlim = c(58, 77), ylim = c(58,80), pch=19, col="blue")
+    abline(v=input$estimated, lwd=2, col="red")
+    fit1 <- lm(y~x)
+    a <- fit1$coefficients[2]
+    b <- fit1$coefficients[1]
+    if (input$checkbox==TRUE){
+        abline(reg=fit1, lwd = 2, col="black")
+        points(input$estimated, a*input$estimated+b, pch=16, cex=2, col="red")
+        }
+    
+    })
+  
+ 
+  output$text1 <- renderText({
+      xmin <- min(input$inmodel)
+      xmax <- max(input$inmodel)
+      fs <- fs[(father.son$fheight>=xmin)&(father.son$fheight<=xmax),]
+      x <- fs$fheight
+      y <- fs$sheight
+      plot(x,y, xlab = "father's height (inches)", ylab = "son's height (inches)", xlim = c(58, 77), ylim = c(58,80), pch=19, col="blue")
+      abline(v=input$estimated, lwd=2, col="red")
+      fit1 <- lm(y~x)
+      newx <- data.frame(x=input$estimated)
+      prd <- predict(fit1, newx, interval="predict")
+      txt1 <- paste("The estimated son's height is: ", prd[1])
+      txt2 <- paste("The 95% confidence interval for the estimated son's height is: ", prd[2], "to ", prd[3])
+      txt1
+  })
+
+  output$text2 <- renderText({
+      xmin <- min(input$inmodel)
+      xmax <- max(input$inmodel)
+      fs <- fs[(father.son$fheight>=xmin)&(father.son$fheight<=xmax),]
+      x <- fs$fheight
+      y <- fs$sheight
+      plot(x,y, xlab = "father's height (inches)", ylab = "son's height (inches)", xlim = c(58, 77), ylim = c(58,80), pch=19, col="blue")
+      abline(v=input$estimated, lwd=2, col="red")
+      fit1 <- lm(y~x)
+      newx <- data.frame(x=input$estimated)
+      prd <- predict(fit1, newx, interval="predict")
+      txt1 <- paste("The estimated son's height is: ", prd[1])
+      txt2 <- paste("The 95% confidence interval for the estimated son's height is: ", prd[2], "to ", prd[3])
+      txt2
+  })
+})
---
+++ ui.R
@@ -0,0 +1,48 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Exploratoy Analysis of Father-Son Dataset"),
+  
+  # Sidebar 
+  sidebarLayout(
+    sidebarPanel(
+      h3("Father-Son height modeling"),
+      p("In this app a simple modeling analysis is done. The objective of this modeling is to estimate the height of a son, given the height of his father."),
+      p("The predictive model is developed by using existing data. The user can specify whether the regression line is displayed or not."),
+      p("Once the user is happy with the performance of the model, they can input the father's height to get an estimate of the son's height by clicking on the button."),
+      
+      sliderInput("inmodel",
+                   "1- Select the range of heights to use for regression:",
+                   min = 59,
+                   max = 76,
+                   value = c(66,70)),
+      checkboxInput("checkbox", "Display regression line", value = TRUE),
+      sliderInput("estimated",
+                  "2- What is the height of the father?",
+                  min = 59,
+                  max = 76,
+                  value = 63),
+      p("The regression line is derived using the selected data range, and changes accordingly. The user can turn the regression line on or off by activating the checkbox"),
+      p("The vertical line shows the value of father's height for which the son's height is predicted. The red dot shows the predicted value."),
+      p("The exact predicted value and 95% confidence interval for the predicted son's height is shown below the chart and is updated automatically")
+    ),
+    
+    # Show a plot
+    mainPanel(
+       plotOutput("distPlot"),
+       verbatimTextOutput("text1"),
+       verbatimTextOutput("text2")
+    )
+  )
+))
